be3dca8db5af18faba50a2fe18ed1f219b19716e
Bryan Jay <120381978+BryanJayHizole@users.noreply.github.com>
2024-05-19 21:13:09 +0800

    commit3

--- functions/api.js
+++ functions/api.js
@@ -1,24 +1,24 @@
-  // api.js
-  const express = require('express');
-  const serverless = require('serverless-http');
-  const router = require('./routes/register');
-  const mongoose = require('mongoose');
-  const cors = require('cors');
+    // api.js
+    const express = require('express');
+    const serverless = require('serverless-http');
+    const router = require('./routes/register');
+    const mongoose = require('mongoose');
+    const cors = require('cors');
 
-  const app = express();
+    const app = express();
 
-  //your mongoDB Cloud URL
-  const dbCloudUrl = 'mongodb+srv://bryanhizole:bryan142001@cluster0.ogfgkgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-  const dbLocalUrl = 'mongodb://localhost:27017/Pawsome';
+    //your mongoDB Cloud URL
+    const dbCloudUrl = 'mongodb+srv://bryanhizole:bryan142001@cluster0.ogfgkgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    const dbLocalUrl = 'mongodb://localhost:27017/Pawsome';
 
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-  mongoose.connect(dbCloudUrl || dbLocalUrl)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((error) => console.error('Failed to connect to MongoDB', error));
+    mongoose.connect(dbCloudUrl || dbLocalUrl, { useNewUrlParser: true, useUnifiedTopology: true }) // Added options for MongoDB connection
+      .then(() => console.log('Connected to MongoDB'))
+      .catch((error) => console.error('Failed to connect to MongoDB', error));
 
-  app.use('/.netlify/functions/api', router);
+    app.use('/.netlify/functions/api', router);
 
-  module.exports.handler = serverless(app);
+    module.exports.handler = serverless(app);
