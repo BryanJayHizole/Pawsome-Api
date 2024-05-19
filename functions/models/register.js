ef70a13a5af4716e2af40957e68078d7ca7eab33
Bryan Jay <120381978+BryanJayHizole@users.noreply.github.com>
2024-05-19 21:16:33 +0800

    commit4

--- functions/models/register.js
+++ functions/models/register.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const registerSchema = require('../schema/register'); // Ensure this path is correct
 
-const PetRegisterModel = mongoose.models.Register || mongoose.model('Register', registerSchema);
+const PetRegisterModel = mongoose.model('Register', registerSchema);
 
 module.exports = PetRegisterModel;
