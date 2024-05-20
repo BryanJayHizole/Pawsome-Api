9391cac9988018979d0f608e9e6a2e2a332ec131
Bryan Jay <120381978+BryanJayHizole@users.noreply.github.com>
2024-05-20 08:47:11 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -8,14 +8,13 @@ const cors = require('cors');
 const app = express();
 
 //your mongoDB Cloud URL
-const dbCloudUrl = 'mongodb+srv://bryanhizole:bryan142001@cluster0.ogfgkgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const dbLocalUrl = 'mongodb://localhost:27017/Pawsome';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(dbCloudUrl || dbLocalUrl)
+mongoose.connect(dbLocalUrl)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Failed to connect to MongoDB', error));
 
