6baddbdb55aaa5a379c515467499479e1758a663
Bryan Jay <120381978+BryanJayHizole@users.noreply.github.com>
2024-05-20 08:49:33 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -8,13 +8,14 @@ const cors = require('cors');
 const app = express();
 
 //your mongoDB Cloud URL
+const dbCloudUrl = 'mongodb+srv://bryanhizole:bryan142001@cluster0.ogfgkgo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const dbLocalUrl = 'mongodb://localhost:27017/Pawsome';
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(dbLocalUrl)
+mongoose.connect(dbCloudUrl || dbLocalUrl)
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Failed to connect to MongoDB', error));
 
